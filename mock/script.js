56beeafb34cac408629fb0291c001fb86370e258
Gil <85408560+Gipeio@users.noreply.github.com>
2023-02-22 10:24:58 +0100

    pausing background animation

--- mock/script.js
+++ mock/script.js
@@ -90,3 +90,16 @@
     
   })();
   
+
+
+
+
+    /* Toggle Animations */
+    const jstoggle = document.getElementById('js-toggle');
+    jstoggle.addEventListener('click', () => {
+      const animations = document.querySelectorAll('[data-animation');
+      animations.forEach(animation => {
+        const running = animation.style.animationPlayState || 'running';
+        animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+      })
+    });
